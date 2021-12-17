80018d7282338325cc120c94908c1587f1cb705d
Giles Roadnight <giles.roadnight@gmail.com>
2021-12-17 15:24:49 +0000

    First node run

--- src/virtual-dimmer.ts
+++ src/virtual-dimmer.ts
@@ -14,17 +14,31 @@ const defaultConfig: IVirtualDimmerConfig = {
     minBrightness: 0,
 };
 
+type VirtualDimmerNode = NodeDef & IVirtualDimmerConfig;
+
 const nodeInit: NodeInitializer = (RED): void => {
-    function VirtualDimmerConstructor(this: Node, partialConfig: NodeDef & Partial<IVirtualDimmerConfig>): void {
-        const config = { ...defaultConfig, ...partialConfig };
+    function VirtualDimmerConstructor(this: Node, config: NodeDef & IVirtualDimmerConfig): void {
+        config.longPressDelay = config.longPressDelay ?? 80;
+        config.updateInterval = config.updateInterval ?? 100;
+        config.maxBrightness = config.maxBrightness ?? 100;
+        config.minBrightness = config.minBrightness ?? 0;
+
         RED.nodes.createNode(this, config);
 
         this.on('input', (msg, send, done) => {
-            console.log(`INPUT`, { msg, send, done });
+            send({ payload: 'hi' });
+
+            if (done) {
+                done();
+            }
         });
     }
 
     RED.nodes.registerType('virtual-dimmer', VirtualDimmerConstructor);
 };
+
+function applyConfigDefaults(): VirtualDimmerNode{
+
+}
 
 module.exports = nodeInit;
